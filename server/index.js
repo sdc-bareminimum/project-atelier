d9643784eeed6ad1e8a1a3e6896404cbfc34b677
gab <guogabbi@gmail.com>
2021-10-10 21:22:11 -0700

    added CORS headers

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const router = require('./router.js')
 
-const port = 3000
+const port = 3030
 const app = express()
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  // res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
+})
 app.use('/api/reviews', router)
 
 app.listen(port, () => {
